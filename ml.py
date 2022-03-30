c3b064598411aa7b6b33eb7b388b6ba0a87680c5
SeraFM <cs04555@uoi.gr>
2022-03-30 16:55:41 +0300

    Update

--- ml.py
+++ ml.py
@@ -4,26 +4,24 @@ import seaborn as sns
 from matplotlib.colors import ListedColormap
 from sklearn import neighbors, datasets
 import csv
+import pandas as pd
 n_neighbors = 40
 
 # import some data to play with
 #iris = datasets.load_iris()
 file = open('train.csv')
-
+field_names=['bone-length','rotting_flesh','hair_length','has-soul','color']
 type(file)
 csvreader = csv.reader(file)
-data=list(csvreader)
-# we only take the first two features. We could avoid this ugly
-# slicing by using a two-dim dataset
-X = data[:5]
-data.pop(0)
-#print(data)
+dataset=pd.read_csv(file)
+print(dataset.head())
 
 
+"""""
 h = 0.02  # step size in the mesh
 color_matrix_names=["clear","green","black","blue","white","blood"]
 color_matrix_values = [0.0,0.0,0.0,0.0,0.0,0.0]
 for i in range(6):
     color_matrix_values[i]=(i+1)/6
-
-print(color_matrix_values)
+clf=neighbors.KNeighborsClassifier(n_neighbors,"distance")
+print(color_matrix_values)"""""
